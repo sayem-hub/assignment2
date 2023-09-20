6ea42cf28401bbd94af2f1e2a645d22158f799ab
Abu Sayem Amir <sayem74@gmail.com>
2023-09-20 08:59:13 +0600

    done even number with for loop

---
+++ assignment2.php
@@ -0,0 +1,24 @@
+<?php
+
+/* Task 1: Looping with Increment using a Function
+Write a PHP function that uses a for loop to print all even numbers from 1 to 20, but with a
+step of 2. In other words, you should print 2, 4, 6, 8, 10, 12, 14, 16, 18, 20. The function
+should take the arguments like start as 1, end as 20 and step as 2. You must call the
+function to print.
+Also do the same using while loop and do-while loop also.*/
+
+function evenNumbers($start_number, $end_number, $step)
+{
+    if ($start_number % 2 != 0) {
+        $start_number++;
+    }
+
+    for ($i = $start_number; $i <= $end_number; $i += $step) {
+        echo $i . ' ';
+    }
+}
+
+evenNumbers(1, 20, 2);
+
+//Output : 2 4 6 8 10 12 14 16 18 20 
+
